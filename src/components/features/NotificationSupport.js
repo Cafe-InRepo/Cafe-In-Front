b8c5da19676f490909f8691ed5ea542cb5f1b325
wsaadli <wsaadli@vermeg.com>
2024-09-10 21:11:01 +0100

    notif support

--- src/components/features/NotificationSupport.js
+++ src/components/features/NotificationSupport.js
@@ -3,7 +3,6 @@ import tw from "twin.macro";
 import styled from "styled-components";
 import { ReactComponent as BasketIconSvg } from "../../images/bill.svg"; // Ensure this path is correct
 import socket from "helpers/soket/socket";
-import { GetToken } from "helpers/GetToken";
 const IconContainer = styled.div`
   ${tw`fixed text-white p-4 rounded-full shadow-lg cursor-pointer bg-primary-500`}
   display: flex;
@@ -16,17 +15,7 @@ const IconContainer = styled.div`
   z-index: 1;
 `;
 
-const Badge = styled.div`
-  ${tw`absolute bg-red-600 text-white rounded-full flex items-center justify-center`}
-  width: 24px; // Adjust size as needed
-  height: 24px; // Adjust size as needed
-  bottom: -10px; // Adjust position as needed
-  left: -10px; // Adjust position as needed
-  font-size: 14px; // Adjust font size as needed
-`;
-
 const NotifSupport = () => {
-  const token = GetToken();
   const callForSupport = (tableNumber) => {
     socket.emit("supportRequest", { tableNumber });
 
